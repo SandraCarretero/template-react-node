0ccad5a939751bfac4a8b477f7cc9dbc4d190b8e
SandraCarretero <sandracarretero24@gmail.com>
2024-07-16 17:17:08 +0200

    update template

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
 // Rutas
+const usersRoutes = require('./routes/users.routes');
 
 // Middlewares para cliente
 // Opciones avanzadas de configuración de CORS
@@ -16,5 +16,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Uso de rutas
+app.use('/api/users', usersRoutes)
 
 app.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
